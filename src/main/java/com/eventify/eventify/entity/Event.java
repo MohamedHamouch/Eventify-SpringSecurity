9b859ce973a33319930dcdc5b6ff9a82456f47c5
majdifox <majmehdi20@gmail.com>
2025-11-19 11:56:47 +0100

    Creating the Event entity

---
+++ src/main/java/com/eventify/eventify/entity/Event.java
@@ -0,0 +1,45 @@
+package com.eventify.eventify.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "event")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "title's field can't be empty")
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank(message = "description's field can't be empty")
+    @Column(nullable = false)
+    private String description;
+
+    @NotBlank(message = "locations's field can't be empty")
+    @Column(nullable = false)
+    private String location;
+
+    @NotNull
+    private LocalDate date;
+
+    @NotNull
+    private Integer capacity;
+
+
+}
