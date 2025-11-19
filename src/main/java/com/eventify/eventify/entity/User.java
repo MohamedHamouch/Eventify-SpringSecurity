fe1e63129cd05dba6f947d9c3591bb56d248a03e
majdifox <majmehdi20@gmail.com>
2025-11-19 12:01:58 +0100

    adding properties to the User entity

--- src/main/java/com/eventify/eventify/entity/User.java
+++ src/main/java/com/eventify/eventify/entity/User.java
@@ -1,15 +1,28 @@
 package com.eventify.eventify.entity;
 
-import jakarta.persistence.Entity;
+import com.eventify.eventify.entity.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "user")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name's field can't be empty")
+    @Column(nullable = false)
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
 
 }
