e0396037bfee1cf11381fe7c64fb371cf0f296b3
Mohamed Hamouch <med10hamouch@gmail.com>
2025-11-20 17:41:09 +0100

    add user DTOs

---
+++ src/main/java/com/eventify/eventify/dto/UserResponse.java
@@ -0,0 +1,15 @@
+package com.eventify.eventify.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String role; // ROLE_USER / ROLE_ADMIN / ROLE_ORGANIZER
+}
