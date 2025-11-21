1f468dbda934ce42fbb1fdb370ef921b872e2477
Mohamed Hamouch <med10hamouch@gmail.com>
2025-11-21 09:17:23 +0100

    add repository query methods

--- src/main/java/com/eventify/eventify/repository/RegistrationRepository.java
+++ src/main/java/com/eventify/eventify/repository/RegistrationRepository.java
@@ -3,5 +3,9 @@ package com.eventify.eventify.repository;
 import com.eventify.eventify.entity.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+    List<Registration> findByUserId(Long userId);
+    boolean existsByUserIdAndEventId(Long userId, Long eventId);
 }
